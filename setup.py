07b84acbd9c4b502dac1aa7978b5d02be1995350
SunnyCodex <sa21270@gmail.com>
2025-10-17 13:12:28 +0530

    data ingestion/ transformation nd utils done

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .'
 
-def requirements(file_path:str)->List[str]:
-    with open(file_path) as file:
-        requirements=file.readlines()
-        requirements=[req.replace('\n','') for req in requirements]
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function returns the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
-
-
 setup(
-    name='Ml_project',
+    name='mlproject',
     version='0.0.1',
-    author='Sunny',
-    author_email='sa2181780@gmail.com',
+    author='Your Name',
+    author_email='your.email@domain.com',
     packages=find_packages(),
-    install_requires=requirements('requirements.txt')
+    install_requires=get_requirements('requirements.txt')
 )
